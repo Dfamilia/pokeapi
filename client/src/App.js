7fc21f665e8c681ffaa208cd59d62fef022fdffd
dfamilia <dfamilia@intellisys.com.do>
2020-09-03 16:28:04 -0400

    add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PokemonListProvider from './Context/pokemonListContext';
+import Navbar from './components/Navbar/index';
 import Hero from './components/Hero/index';
-
 const App = () => {
 
   return (
     <PokemonListProvider>
+      <Navbar />
       <Hero />
     </PokemonListProvider>
   );
